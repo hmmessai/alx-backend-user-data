fa2c43d816624b386b683a28581b9305ce079598
Messai <hmmessai@github.comm>
2023-11-07 20:14:34 +0000

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """Defines BasicAuth class
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -18,4 +19,19 @@ class BasicAuth(Auth):
             if auth_scheme[0] == 'Basic':
                 return auth_scheme[1]
             return None
+        return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """Decodes the base64 authorization header
+        """
+        b64_auth_head = base64_authorization_header
+        if b64_auth_head and type(b64_auth_head) == str:
+            try:
+                decoded_value = base64.b64decode(b64_auth_head)
+                decoded_value = decoded_value.decode('utf-8')
+            except Exception:
+                return None
+            return decoded_value
         return None
