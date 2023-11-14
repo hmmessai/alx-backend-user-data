d4dcda09e0ecae9f2431a804c118b1f0e66d5f82
Messai <hmmessai@github.comm>
2023-11-14 13:45:29 +0000

    task 9

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,17 +4,24 @@
 import bcrypt
 from uuid import uuid4
 from sqlalchemy.orm.exc import NoResultFound
-from typing import (TypeVar,Union)
+from typing import (TypeVar, Union)
 from db import DB
 from user import User
 
 U = TypeVar(User)
+
 
 def _hash_password(password: str) -> bytes:
     """Hashes a password
     """
     passwd = password.encode('utf-8')
     return bcrypt.hashpw(passwd, bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """Generates uuid
+    """
+    return str(uuid4())
 
 
 class Auth:
