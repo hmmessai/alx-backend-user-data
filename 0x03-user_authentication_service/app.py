bae01ccc3af0c0ceeaab22df4a4d6984ecb1777c
Messai <hmmessai@github.comm>
2023-11-14 14:10:09 +0000

    task 10

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -13,6 +13,7 @@ AUTH = Auth()
 def home():
     return jsonify({"message": "Bienvenue"})
 
+
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users():
     """implements /users route
@@ -24,7 +25,6 @@ def users():
         return jsonify({"email": email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
-
 
 
 if __name__ == "__main__":
