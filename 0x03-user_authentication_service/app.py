c34f30ece2a8460054458581e85706505f52e383
Messai <hmmessai@github.comm>
2023-11-14 14:46:22 +0000

    task 14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -34,12 +34,25 @@ def login():
     email = request.form["email"]
     password = request.form["password"]
     if AUTH.valid_login(email, password):
-       session_id =  AUTH.create_session(email)
-       resp = jsonify({"email": email, "message": "logged in"})
-       resp.set_cookie("session_id", session_id)
-       return resp
+        session_id = AUTH.create_session(email)
+        resp = jsonify({"email": email, "message": "logged in"})
+        resp.set_cookie("session_id", session_id)
+        return resp
     else:
-       abort(401)
+        abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """Implements logout
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
 
 
 if __name__ == "__main__":
